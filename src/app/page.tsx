1d5df225926edd62c57a9b2d73f706ff56027dfd
sidheshsarda <137500256+sidheshsarda@users.noreply.github.com>
2025-01-11 15:37:53 +0530

    Update page.tsx (#5)
    * Update page.tsx
    
    * Update deploy.yml
    
    * Update deploy.yml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Dev CHanges deployment check.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">

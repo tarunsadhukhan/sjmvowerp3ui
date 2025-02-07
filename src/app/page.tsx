a16a9d36fc0f38d53ad3d4778c56ac45eb0cba63
sidheshsarda <137500256+sidheshsarda@users.noreply.github.com>
2025-02-07 12:35:37 +0530

    Update page.tsx (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <li className="mb-2">
             Dev{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              rev1
             </code>
             .
           </li>
